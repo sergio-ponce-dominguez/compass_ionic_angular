a5a3ea27ffba87a351cd17a498099983e89e237d
sergio <sergio0ponce0dominguez@gmail.com>
2025-05-12 17:30:13 +0200

    feat: rotate compass according to deviceorientationabsolute event

--- src/app/compass/compass.page.ts
+++ src/app/compass/compass.page.ts
@@ -1,24 +1,50 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
   IonContent,
+  IonFab,
+  IonHeader,
+  IonText,
+  IonTitle,
+  IonToolbar,
 } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-compass',
   templateUrl: 'compass.page.html',
   styleUrls: ['compass.page.scss'],
   imports: [
+    IonFab,
+    IonText,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
-    ExploreContainerComponent,
+    DecimalPipe,
   ],
 })
-export class CompassPage {
-  constructor() {}
+export class CompassPage implements OnInit, OnDestroy {
+  constructor(private platform: Platform) {}
+
+  event: DeviceOrientationEvent | null = null;
+  imageTransform = 'rotate(0deg)';
+
+  ngOnInit(): void {
+    this.platform.ready().then(() => {
+      window.addEventListener('deviceorientationabsolute', this.handleEvent);
+    });
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('deviceorientationabsolute', this.handleEvent);
+  }
+
+  private handleEvent = (event: DeviceOrientationEvent): void => {
+    this.event = event;
+    if (event.alpha === null) {
+      return;
+    }
+    this.imageTransform = `rotate(${event.alpha}deg)`;
+  };
 }
